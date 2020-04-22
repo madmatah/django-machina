26a07b741f3449c467f4de9871e5921be42824ce
Matthieu Huguet <matthieu.huguet@fun-mooc.fr>
2020-04-22 18:07:59 +0200

    Add MACHINA_MARKUP_MAX_LENGTH_VALIDATOR setting

--- machina/core/validators.py
+++ machina/core/validators.py
@@ -1,4 +1,10 @@
 from django.core import validators
+from django.core.exceptions import ImproperlyConfigured
+from django.utils.deconstruct import deconstructible
+from django.utils.encoding import smart_str
+from django.utils.translation import gettext_lazy as _
+
+from machina.conf import settings as machina_settings
 
 
 class NullableMaxLengthValidator(validators.MaxLengthValidator):
@@ -11,3 +17,34 @@ class NullableMaxLengthValidator(validators.MaxLengthValidator):
             # performed.
             return
         super().__call__(value)
+
+
+@deconstructible
+class MarkupMaxLengthValidator:
+    """
+    Validates the max length of an input written in the markup language defined in the settings.
+
+    This is a proxy that use the implementation defined in MACHINA_MARKUP_MAX_LENGTH_VALIDATOR
+    setting.
+    """
+
+    def __init__(self, limit_value):
+        self.validator = self._get_markup_maxlength_validator(limit_value)
+
+    def __call__(self, value):
+        self.validator(value)
+
+    @staticmethod
+    def _get_markup_maxlength_validator(max_length):
+        dotted_path = machina_settings.MARKUP_MAX_LENGTH_VALIDATOR
+        try:
+            module, validator = dotted_path.rsplit('.', 1)
+            module, validator = smart_str(module), smart_str(validator)
+            validator = getattr(__import__(module, {}, {}, [validator]), validator)
+            return validator(max_length)
+        except ImportError as e:
+            raise ImproperlyConfigured(
+                _('Could not import MARKUP_MAX_LENGTH_VALIDATOR {}: {}').format(
+                    machina_settings.MARKUP_MAX_LENGTH_VALIDATOR, e
+                )
+            )
--- tests/unit/core/test_validators.py
+++ tests/unit/core/test_validators.py
@@ -1,7 +1,8 @@
 import pytest
-from django.core.exceptions import ValidationError
+from django.core.exceptions import ImproperlyConfigured, ValidationError
 from faker import Faker
 
+from machina.conf import settings as machina_settings
 from machina.core import validators
 
 
@@ -17,3 +18,27 @@ class TestNullableMaxLengthValidator(object):
         assert validator_1(faker.text()) is None
         with pytest.raises(ValidationError):
             validator_2('test' * 10)
+
+
+class TestMarkupMaxLengthValidator(object):
+    def test_default_validator(self):
+        # Setup
+        validator = validators.MarkupMaxLengthValidator(None)
+        # Run & check
+        assert validator(faker.text()) is None
+
+    def test_custom_validator(self):
+        # Setup
+        machina_settings.MARKUP_MAX_LENGTH_VALIDATOR = 'django.core.validators.MaxLengthValidator'
+        validator = validators.MarkupMaxLengthValidator(2)
+        # Run & check
+        assert validator('aa') is None
+        with pytest.raises(ValidationError):
+            validator('aaa')
+
+    def test__should_not_allow_non_accessible_validator(self):
+        # Setup
+        machina_settings.MARKUP_MAX_LENGTH_VALIDATOR = 'it.will.fail.Validator'
+        # Run & check
+        with pytest.raises(ImproperlyConfigured):
+            validators.MarkupMaxLengthValidator(2)
